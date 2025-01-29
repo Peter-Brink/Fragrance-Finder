db0814fd8b6171beba28071ca393b08156740fb7
Petrus Brink <peterjbrink@gmail.com>
2025-01-29 22:00:47 +1100

    Added man and woman emojis.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    // Adding @svgr/webpack loader to Next.js webpack config
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
