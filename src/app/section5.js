da27558a4469547302a5884e980a8ba0a233e615
Petrus Brink <peterjbrink@gmail.com>
2025-01-30 18:14:39 +1100

    Added occasion selector to section 5.

--- src/app/section5.js
+++ src/app/section5.js
@@ -1,6 +1,7 @@
 import FadingDiv from "@/components/visual/components/nav-arrow-down";
 import HeadingText from "@/components/visual/components/heading-text";
 import NavArrowUp from "@/components/visual/components/nav-arrow-up";
+import OccasionSelection from "@/components/visual/occasion-selection";
 
 const Section5 = () => {
   return (
@@ -9,8 +10,9 @@ const Section5 = () => {
       className="section bg-gradient-to-b from-myOrange to-myYellow text-white snap-start"
     >
       <NavArrowUp section={5} />
-      <FadingDiv section={5} />
       <HeadingText text={"What occasion is your fragrance for?"} />
+      <OccasionSelection />
+      <FadingDiv section={5} />
     </section>
   );
 };
---
+++ src/components/visual/occasion-selection.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import SelectionBlock from "./components/selection-block";
+import EverydayEmoji from "../../../public/emojis/everyday.svg";
+import NightEmoji from "../../../public/emojis/night.svg";
+import BeachEmoji from "../../../public/emojis/beach.svg";
+import SportEmoji from "../../../public/emojis/sport.svg";
+import WorkEmoji from "../../../public/emojis/work.svg";
+import DateEmoji from "../../../public/emojis/date.svg";
+
+const OccasionSelection = () => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const options = [
+    {
+      label: "Everyday",
+      icon: <EverydayEmoji className="w-[50%] h-auto" />,
+    },
+    { label: "Night out", icon: <NightEmoji className="w-[50%] h-auto" /> },
+    { label: "Beach day", icon: <BeachEmoji className="w-[50%] h-auto" /> },
+    {
+      label: "Sports/gym",
+      icon: <SportEmoji className="w-[50%] h-auto" />,
+    },
+    {
+      label: "Work/office",
+      icon: <WorkEmoji className="w-[50%] h-auto pt-1" />,
+    },
+    { label: "Date night", icon: <DateEmoji className="w-[50%] h-auto" /> },
+  ];
+
+  const handleSelect = (label) => {
+    setSelectedOptions((prev) =>
+      prev.includes(label)
+        ? prev.filter((item) => item !== label)
+        : [...prev, label]
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center w-[100vw] mt-6">
+      <div className="grid grid-cols-2  sm:grid-cols-3 gap-2 sm:gap-5 md:gap-10 lg:gap-x-20">
+        {options.map(({ label, icon }) => (
+          <SelectionBlock
+            key={label}
+            label={label}
+            isSelected={selectedOptions.includes(label)}
+            onSelect={() => handleSelect(label)}
+          >
+            {icon}
+          </SelectionBlock>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OccasionSelection;
