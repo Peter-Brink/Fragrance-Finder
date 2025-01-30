ff6ebb6a5e19e7c2347981e4dff519be41497fc7
Petrus Brink <peterjbrink@gmail.com>
2025-01-30 22:11:15 +1100

    Added built wit love sign.

--- src/app/landing-page.js
+++ src/app/landing-page.js
@@ -1,4 +1,5 @@
 import FadingDiv from "@/components/visual/components/nav-arrow-down";
+import { FaGithub } from "react-icons/fa";
 
 const LandingPage = () => {
   return (
@@ -6,6 +7,17 @@ const LandingPage = () => {
       id="section0"
       className="section justify-center snap-start bg-gradient-to-b from-myNavy via-myNavy to-myBlue"
     >
+      <p className="absolute top-5 right-5 text-white text-xs sm:text-sm flex items-center gap-1">
+        Built with ❤️ by{" "}
+        <a
+          href="https://github.com/Peter-Brink"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline flex items-center gap-1"
+        >
+          Petrus <FaGithub className="text-xl" />
+        </a>
+      </p>
       <div className="flex flex-col items-center mb-10">
         <h1 className="text-2xl xs:text-4xl sm:text-5xl md:text-6xl font-sans font-light text-white mb-5">
           Your fragrance helps you
