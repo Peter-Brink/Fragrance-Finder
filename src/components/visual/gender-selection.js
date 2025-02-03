022599fa61e5067e55f3bae6a26998800120d23b
Petrus Brink <peterjbrink@gmail.com>
2025-02-03 13:42:31 +1100

    Fixed gender selection so that it uses Zustand store properly.

--- src/components/visual/gender-selection.js
+++ src/components/visual/gender-selection.js
@@ -3,31 +3,25 @@ import WomanEmoji from "../../../public/emojis/woman.svg";
 import useUserInputStore from "@/store/useUserInputStore";
 
 const GenderSelection = () => {
-  const { section2, setSectionData } = useUserInputStore();
-
-  const handleSelection = (gender) => {
-    setSectionData("section2", gender);
-  };
+  const { gender, setSectionData } = useUserInputStore();
 
   return (
     <div className="flex items-center justify-center gap-5 xs:gap-10 lg:gap-20 w-[100%] h-[60vh]">
       <div
         className={`gender-block ${
-          section2 == "male"
-            ? "outline outline-4 outline-myNavy"
-            : "outline-none"
+          gender == "male" ? "outline outline-4 outline-myNavy" : "outline-none"
         }`}
-        onClick={() => handleSelection("male")}
+        onClick={() => setSectionData("gender", "male")}
       >
         <ManEmoji className="w-[50%] h-auto" />
       </div>
       <div
         className={`gender-block ${
-          section2 == "female"
+          gender == "female"
             ? "outline outline-4 outline-myNavy"
             : "outline-none"
         }`}
-        onClick={() => handleSelection("female")}
+        onClick={() => setSectionData("gender", "female")}
       >
         <WomanEmoji className="w-[50%] h-auto ml-3 xs:ml-5 md:ml-10" />
       </div>
