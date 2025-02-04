50ac94ef40a5b738a966d86455ad2c5a45ca4632
Petrus Brink <peterjbrink@gmail.com>
2025-02-04 16:18:16 +1100

    Changed cors structure.

--- pages/api/llm-connector.js
+++ pages/api/llm-connector.js
@@ -1,20 +1,15 @@
 export default async function handler(req, res) {
-  if (process.env.VERCEL_BRANCH_URL) {
-    console.log(process.env.VERCEL_BRANCH_URL);
-  } else {
-    console.log("No VERCEL_BRANCH_URL");
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_BRANCH_URL}`
-      : process.env.NEXT_PUBLIC_API_BASE_URL
-  );
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
+    console.log("preflight request received");
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      process.env.NODE_ENV === "production"
+        ? `https://${process.env.VERCEL_BRANCH_URL}`
+        : process.env.NEXT_PUBLIC_API_BASE_URL
+    );
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     return res.status(200).end();
   }
 
--- src/utils/api-connector.js
+++ src/utils/api-connector.js
@@ -63,6 +63,7 @@ export const makeApiCall = async () => {
         body: JSON.stringify({
           message: llmInput,
         }),
+        mode: "cors",
       }
     );
 
